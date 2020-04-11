35f795a2c2c2e6977963e2bb9e1e4be1189ef5c1
BuildTools <thatziv@gmail.com>
2020-04-10 20:06:32 -0400

    final updates
    All future routes have been suspended until the TSD reopens.

--- gallery/order.py
+++ gallery/order.py
@@ -2,14 +2,15 @@ import os
 import sys
 
 def main(): 
-    startNumber = 1
+    startNumber = 21 # aka the number you LEFT ON last time
       
-    for filename in os.listdir("./"): 
-        if filename != sys.argv[0]:
+    for filename in os.listdir("./"):
+        # checks to see if the filename is a number and if the file isnt the script it's self
+        if filename != sys.argv[0] and not filename.split(".")[0].isdigit():
+            startNumber += 1
             dst = str(startNumber) + ".jpg"
             src = filename 
             os.rename(src, dst) 
-            startNumber += 1
   
 # Driver Code 
 if __name__ == '__main__': 
